c2889dd781fe50a8b454321aebfa23348a360b32
Wildhoney <adam.timberlake@gmail.com>
2014-05-13 23:02:56 +0100

    Added sorting tests

--- tests/spec.js
+++ tests/spec.js
@@ -1,76 +1,114 @@
 describe('ngPourOver', function() {
 
-    beforeEach(module('ngPourOver'));
+    var $pourOver, $filter;
 
-    describe('Filtering', function() {
+    beforeEach(function() {
 
-        var $pourOver, $filter;
+        module('ngPourOver');
 
-        beforeEach(function() {
+        inject(function(_PourOver_, _$filter_) {
 
-            inject(function(_PourOver_, _$filter_) {
+            var collection = [
+                { name: 'Adam',  friends: ['Maria', 'Artem'] },
+                { name: 'Maria', friends: ['Adam'] },
+                { name: 'Artem', friends: ['Adam'] }];
 
-                var collection = [
-                    { name: 'Adam',  friends: ['Maria', 'Artem'] },
-                    { name: 'Maria', friends: ['Adam'] },
-                    { name: 'Artem', friends: ['Adam'] }];
-
-                $pourOver      = new _PourOver_(collection);
-                $filter        = _$filter_;
-
-            });
+            $pourOver      = new _PourOver_(collection);
+            $filter        = _$filter_;
 
         });
 
-        it('Should be able to add an exact filter.', function() {
-            expect($pourOver._collection.filters.name).toBeUndefined();
-            $pourOver.addExactFilter('name');
-            expect($pourOver._collection.filters.name).toBeDefined();
+    });
+
+    it('Should be able to add an exact filter.', function() {
+        expect($pourOver._collection.filters.name).toBeUndefined();
+        $pourOver.addExactFilter('name');
+        expect($pourOver._collection.filters.name).toBeDefined();
+    });
+
+    it('Should be able to add an inclusive filter.', function() {
+        expect($pourOver._collection.filters.name).toBeUndefined();
+        $pourOver.addInclusionFilter('name');
+        expect($pourOver._collection.filters.name).toBeDefined();
+    });
+
+    it('Should be able to fetch the unique properties.', function() {
+        expect($pourOver._fetchProperties('name').length).toEqual(3);
+    });
+
+    it('Should be able to sort by the name property.', function() {
+
+        var collection;
+
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(3);
+
+        expect($pourOver._sortBy).toEqual(null);
+        $pourOver.addSort('name', {
+            attr: 'name',
+            fn: function(a, b) {
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+                return 0;
+            }
         });
+        $pourOver.sortBy('name');
+        expect($pourOver._sortBy).not.toEqual(null);
 
-        it('Should be able to add an inclusive filter.', function() {
-            expect($pourOver._collection.filters.name).toBeUndefined();
-            $pourOver.addInclusionFilter('name');
-            expect($pourOver._collection.filters.name).toBeDefined();
-        });
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(3);
+        expect(collection[0].name).toEqual('Adam');
+        expect(collection[1].name).toEqual('Artem');
+        expect(collection[2].name).toEqual('Maria');
 
-        it('Should be able to fetch the unique properties.', function() {
-            expect($pourOver._fetchProperties('name').length).toEqual(3);
-        });
+        $pourOver.unsort();
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(3);
+        expect(collection[0].name).toEqual('Adam');
+        expect(collection[1].name).toEqual('Maria');
+        expect(collection[2].name).toEqual('Artem');
 
-        it('Should be able to exact filter by name property.', function() {
+    });
 
-            var collection;
+    it('Should be able to exact filter by name property.', function() {
 
-            collection = $filter('poCollection')($pourOver);
-            expect(collection.length).toEqual(3);
+        var collection;
 
-            expect($pourOver._filters.name).toBeUndefined();
-            $pourOver.addExactFilter('name');
-            $pourOver.filterBy('name', 'Adam');
-            expect($pourOver._filters.name).toBeDefined();
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(3);
 
-            collection = $filter('poCollection')($pourOver);
-            expect(collection.length).toEqual(1);
+        expect($pourOver._filters.name).toBeUndefined();
+        $pourOver.addExactFilter('name');
+        $pourOver.filterBy('name', 'Adam');
+        expect($pourOver._filters.name).toBeDefined();
 
-        });
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(1);
 
-        it('Should be able to inclusive filter by friends property.', function() {
+        $pourOver.unfilterBy('name');
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(3);
 
-            var collection;
+    });
 
-            collection = $filter('poCollection')($pourOver);
-            expect(collection.length).toEqual(3);
+    it('Should be able to inclusive filter by friends property.', function() {
 
-            expect($pourOver._filters.friends).toBeUndefined();
-            $pourOver.addInclusionFilter('friends');
-            $pourOver.filterBy('friends', 'Adam');
-            expect($pourOver._filters.friends).toBeDefined();
+        var collection;
 
-            collection = $filter('poCollection')($pourOver);
-            expect(collection.length).toEqual(2);
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(3);
 
-        });
+        expect($pourOver._filters.friends).toBeUndefined();
+        $pourOver.addInclusionFilter('friends');
+        $pourOver.filterBy('friends', 'Adam');
+        expect($pourOver._filters.friends).toBeDefined();
+
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(2);
+
+        $pourOver.unfilterBy('friends');
+        collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(3);
 
     });
 
