1028364c516120ae85edb5575167444e90dbc410
Wildhoney <adam.timberlake@gmail.com>
2014-05-15 11:42:37 +0100

    Added more tests

--- tests/spec.js
+++ tests/spec.js
@@ -11,9 +11,12 @@ describe('ngPourOver', function() {
             var collection = [
                 { name: 'Adam',  friends: ['Maria', 'Artem'] },
                 { name: 'Maria', friends: ['Adam'] },
-                { name: 'Artem', friends: ['Adam'] }];
+                { name: 'Artem', friends: ['Adam'] },
+                { name: 'Sarah', friends: ['Adam', 'Maria'] },
+                { name: 'Karl',  friends: ['Adam', 'Maria'] }];
 
             $pourOver = new _PourOver_(collection);
+            $pourOver.setPerPage(3);
             $filter   = _$filter_;
 
         });
@@ -33,7 +36,7 @@ describe('ngPourOver', function() {
     });
 
     it('Should be able to fetch the unique properties.', function() {
-        expect($pourOver._fetchProperties('name').length).toEqual(3);
+        expect($pourOver._fetchProperties('name').length).toEqual(5);
     });
 
     it('Should be able to sort by the name property.', function() {
@@ -49,14 +52,16 @@ describe('ngPourOver', function() {
             if (a.name > b.name) return 1;
             return 0;
         });
+
         $pourOver.sortBy('name');
+        expect($pourOver._sortAscending).toEqual(false);
         expect($pourOver._sortBy).not.toEqual(null);
 
         collection = $filter('poCollection')($pourOver);
         expect(collection.length).toEqual(3);
         expect(collection[0].name).toEqual('Adam');
         expect(collection[1].name).toEqual('Artem');
-        expect(collection[2].name).toEqual('Maria');
+        expect(collection[2].name).toEqual('Karl');
 
         $pourOver.unsort();
         collection = $filter('poCollection')($pourOver);
@@ -64,6 +69,17 @@ describe('ngPourOver', function() {
         expect(collection[0].name).toEqual('Adam');
         expect(collection[1].name).toEqual('Maria');
         expect(collection[2].name).toEqual('Artem');
+
+        $pourOver.sortBy('name');
+        $pourOver.sortBy('name');
+        expect($pourOver._sortAscending).toEqual(true);
+        collection = $filter('poCollection')($pourOver);
+        expect(collection[0].name).toEqual('Karl');
+
+        $pourOver.sortBy('name', false);
+        expect($pourOver._sortAscending).toEqual(false);
+        collection = $filter('poCollection')($pourOver);
+        expect(collection[0].name).toEqual('Adam');
 
     });
 
@@ -97,11 +113,11 @@ describe('ngPourOver', function() {
 
         expect($pourOver._filters.friends).toBeUndefined();
         $pourOver.addInclusionFilter('friends');
-        $pourOver.filterBy('friends', 'Adam');
+        $pourOver.filterBy('friends', 'Artem');
         expect($pourOver._filters.friends).toBeDefined();
 
         collection = $filter('poCollection')($pourOver);
-        expect(collection.length).toEqual(2);
+        expect(collection.length).toEqual(1);
 
         $pourOver.unfilter();
         collection = $filter('poCollection')($pourOver);
@@ -109,9 +125,52 @@ describe('ngPourOver', function() {
 
     });
 
-    it('Should be able to move to the next page', function() {
+    it('Should be able to paginate the collection.', function() {
 
+        expect($pourOver._perPage).toEqual(3);
+        expect($pourOver._pageNumber).toEqual(1);
+        $pourOver.nextPage();
+        expect($pourOver._pageNumber).toEqual(2);
+        $pourOver.previousPage();
+        expect($pourOver._pageNumber).toEqual(1);
+        $pourOver.setPageNumber(2);
+        expect($pourOver._pageNumber).toEqual(2);
 
+    });
+
+    it('Should be able to define debug mode.', function() {
+
+        expect($pourOver._debug).toBeFalsy();
+        $pourOver.setDebug(true);
+        expect($pourOver._debug).toBeTruthy();
+        $pourOver.setDebug(false);
+        expect($pourOver._debug).toBeFalsy();
+
+    });
+
+    it ('Should be able to cache the collection.', function() {
+
+        $pourOver.addExactFilter('name');
+        $pourOver.filterBy('name', 'Adam');
+        expect($pourOver._filters.name).toBeDefined();
+
+        expect($pourOver._collectionCache.length).toEqual(0);
+        var collection = $filter('poCollection')($pourOver);
+        expect(collection.length).toEqual(1);
+        expect(collection[0].name).toEqual($pourOver._collectionCache[0].name);
+
+    });
+
+    it ('Should be able to manage the iterations.', function() {
+
+        expect($pourOver._lastIteration).toEqual(0);
+        expect($pourOver._currentIteration).toEqual(1);
+        $pourOver.addExactFilter('name');
+        $pourOver.filterBy('name', 'Adam');
+        expect($pourOver._lastIteration).toEqual(0);
+        expect($pourOver._currentIteration).toEqual(2);
+        $filter('poCollection')($pourOver);
+        expect($pourOver._lastIteration).toEqual(2);
 
     });
 
